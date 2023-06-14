2a472259db4bf4f272355ce52f305934a7ad18ea
root <root@Yusuf.localdomain>
2023-06-14 11:07:52 +0100

    added

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    // Send a GET request to the server and update the div#api_status class
+    $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
+        if (data.status === 'OK') {
+            $('#api_status').addClass('available');
+        } else {
+            $('#api_status').removeClass('available');
+        }
+    });
+});
